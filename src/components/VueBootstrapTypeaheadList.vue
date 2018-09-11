3f0b8b08f4eec2731809dfc9f484087e44c5036c
Ryan Mott <ryan.mott@vml.com>
2018-09-11 10:59:06 -0500

    disable hard-coded sort

--- src/components/VueBootstrapTypeaheadList.vue
+++ src/components/VueBootstrapTypeaheadList.vue
@@ -86,14 +86,16 @@ export default {
       // Filter, sort, and concat
       return this.data
         .filter(i => i.text.match(re) !== null)
-        .sort((a, b) => {
-          const aIndex = a.text.indexOf(a.text.match(re)[0])
-          const bIndex = b.text.indexOf(b.text.match(re)[0])
 
-          if (aIndex < bIndex) { return -1 }
-          if (aIndex > bIndex) { return 1 }
-          return 0
-        }).slice(0, this.maxMatches)
+      /*        .sort((a, b) => {
+                const aIndex = a.text.indexOf(a.text.match(re)[0])
+                const bIndex = b.text.indexOf(b.text.match(re)[0])
+
+                if (aIndex < bIndex) { return -1 }
+                if (aIndex > bIndex) { return 1 }
+                return 0
+              }) */
+        .slice(0, this.maxMatches)
     }
   },
 
